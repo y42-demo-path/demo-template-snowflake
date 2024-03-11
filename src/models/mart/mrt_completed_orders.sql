bcdb4a6a909d8fe0e8efcad4dc763d2ef58d2be6
Rob de Wit <rob.dewit@y42.com>
2024-03-11 15:04:33 +0100

    transform: add mrt models and exposure

---
+++ src/models/mart/mrt_completed_orders.sql
@@ -0,0 +1,51 @@
+WITH
+
+stg_orders AS (
+	SELECT * FROM {{ ref('stg_orders') }}
+),
+
+stg_customers AS (
+	SELECT * FROM {{ ref('stg_customers') }}
+),
+
+stg_payments AS (
+	SELECT * FROM {{ ref('stg_payments') }}
+),
+
+stg_exoplanets AS (
+	SELECT * FROM {{ ref('stg_exoplanets') }}
+)
+
+
+SELECT
+	DATE(ord.order_received) AS order_date,
+	ord.order_received,
+	ord.order_id,
+	pay.amount_total,
+	ord.fulfillment_method,
+	ord.service_station,
+	exo.system_distance AS distance,
+	pay.payment_id,
+	pay.payment_method,
+	cus.customer_id,
+	cus.first_name,
+	cus.last_name,
+	ord.line_items
+
+FROM stg_orders AS ord
+
+LEFT JOIN stg_customers AS cus
+	ON ord.customer_id = cus.customer_id
+
+LEFT JOIN stg_payments AS pay
+	ON ord.order_id = pay.order_id
+
+LEFT JOIN stg_exoplanets AS exo
+	ON ord.service_station = exo.exoplanet_name
+
+WHERE
+	TRUE
+	AND ord.order_status = "ACCEPTED"
+	AND pay.payment_status = "ACCEPTED"
+
+ORDER BY ord.order_received DESC
---
+++ src/models/mart/mrt_revenue_by_payment_method.sql
@@ -0,0 +1,29 @@
+WITH
+
+stg_orders AS (
+    SELECT * FROM {{ ref('stg_orders') }}
+),
+
+stg_payments AS (
+    SELECT * FROM {{ ref('stg_payments') }}
+)
+
+
+SELECT
+    pay.payment_method AS payment_method,
+    DATE(ord.order_received) AS order_date,
+    SUM(pay.amount_total) AS gross_revenue,
+    SUM(IF(ord.order_status = "ACCEPTED" AND pay.payment_status = "ACCEPTED", pay.amount_total, 0)) AS net_revenue
+
+FROM stg_orders AS ord
+
+LEFT JOIN stg_payments AS pay
+    ON ord.order_id = pay.order_id
+
+WHERE
+    TRUE
+    AND pay.payment_method IS NOT NULL
+
+GROUP BY order_date, pay.payment_method
+
+ORDER BY order_date DESC, pay.payment_method ASC

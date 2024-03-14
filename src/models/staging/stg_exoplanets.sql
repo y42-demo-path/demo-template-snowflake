f00d3e83c4ea43d45b242765bf0c01de79be24d0
Rob de Wit <rob.dewit@y42.com>
2024-03-14 16:06:30 +0100

    transform: stg_exoplanets

---
+++ src/models/staging/stg_exoplanets.sql
@@ -0,0 +1,37 @@
+WITH
+
+source AS (
+	SELECT * FROM {{ source('raw_exoplanets', 'exoplanets') }}
+),
+
+renamed AS (
+	SELECT
+		"name" AS exoplanet_name,
+		"binaryflag" AS binary_flag,
+		"mass",
+		"radius",
+		"period",
+		"semimajoraxis" AS semi_major_axis,
+		"eccentricity",
+		"periastron",
+		"longitude",
+		"ascendingnode" AS ascending_node,
+		"inclination",
+		"temperature",
+		"age",
+		"discoverymethod" AS discovery_method,
+		"discoveryyear" AS discovery_year,
+		"lastupdate" AS last_updated,
+		"system_rightascension" AS system_right_ascension,
+		"system_declination",
+		"system_distance",
+		"hoststar_mass",
+		"hoststar_radius",
+		"hoststar_metallicity",
+		"hoststar_temperature",
+		"hoststar_age",
+		"list" AS parent_lists
+	FROM source
+)
+
+SELECT * FROM renamed

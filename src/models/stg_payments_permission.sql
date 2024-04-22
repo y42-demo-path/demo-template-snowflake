d5af0b047523beff563d4a04e5c5d85ed21c3739
Octavian Zarzu <octavian.zarzu@y42.com>
2024-04-22 12:46:08 +0300

    checkpoint

---
+++ src/models/stg_payments_permission.sql
@@ -0,0 +1,21 @@
+{{ config(
+  post_hook = "grant select on {{ this }}"
+) }}
+
+WITH
+
+source AS (
+	SELECT * FROM {{ source('raw_space_fuel_shop', 'public_payments') }}
+),
+
+renamed AS (
+	SELECT
+		"ID" AS payment_id,
+		"ORDER_ID" AS order_id,
+		"AMOUNT_TOTAL" AS amount_total,
+		"PAYMENT_METHOD" AS payment_method,
+		"PAYMENT_STATUS" AS payment_status
+	FROM source
+)
+
+SELECT * FROM renamed
